f10bda735c5e8973aa1c0854a738e88e8e040c75
ao0000 <stsshe.s1925@gmail.com>
2022-03-25 08:14:34 +0900

    add chapter05

---
+++ chapter05/fflush/nofflush.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<unistd.h>
+
+#define N 2000
+
+int main(void){
+    int i;
+
+    for(i = 0; i < N; i++){
+        printf("%d, ", i);
+        usleep(5000);
+    }
+    return 0;
+}
